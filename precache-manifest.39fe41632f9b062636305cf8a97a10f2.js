cdc205118d39c77b62869271b0fe4df7742de1d9
Jenkins <jenkins@common-infra-jenkins-1.asia-south1-c.c.crio-vpn-common.internal>
2020-09-07 10:39:33 +0530

    Updates

---
+++ precache-manifest.39fe41632f9b062636305cf8a97a10f2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0582c94172f716d627c5e477571b67f",
+    "url": "/Crio-PhonePe-Aug-2020-vinay-arya-phonepe/index.html"
+  },
+  {
+    "revision": "fe8f9cf2bde2ddf6a508",
+    "url": "/Crio-PhonePe-Aug-2020-vinay-arya-phonepe/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "2cb9876b01ecb8c84233",
+    "url": "/Crio-PhonePe-Aug-2020-vinay-arya-phonepe/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "fe8f9cf2bde2ddf6a508",
+    "url": "/Crio-PhonePe-Aug-2020-vinay-arya-phonepe/static/js/2.b333617d.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/Crio-PhonePe-Aug-2020-vinay-arya-phonepe/static/js/2.b333617d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cb9876b01ecb8c84233",
+    "url": "/Crio-PhonePe-Aug-2020-vinay-arya-phonepe/static/js/main.000953ca.chunk.js"
+  },
+  {
+    "revision": "68a366db400528c2c0ba",
+    "url": "/Crio-PhonePe-Aug-2020-vinay-arya-phonepe/static/js/runtime-main.c064e63f.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/Crio-PhonePe-Aug-2020-vinay-arya-phonepe/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/Crio-PhonePe-Aug-2020-vinay-arya-phonepe/static/media/crio-logo.63be71d0.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Crio-PhonePe-Aug-2020-vinay-arya-phonepe/precache-manifest.39fe41632f9b062636305cf8a97a10f2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Crio-PhonePe-Aug-2020-vinay-arya-phonepe/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
